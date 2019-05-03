3da69713313170aff342f49e5f03615bc3e8c34e
AruXc <aruxc6030@gmail.com>
2019-05-03 10:59:41 +0800

    connect to MongoDB

--- app.js
+++ app.js
@@ -2,21 +2,33 @@ const express = require('express')
 const bodyPaser = require('body-parser')
 const graphqlHttp = require('express-graphql')
 const { buildSchema } = require('graphql')
-
+const mongoose = require('mongoose')
 const app = express()
+
+const Event = require('./models/event')
 
 app.use(bodyPaser.json())
 
-
 const schema = buildSchema(`
+  type Event {
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
   type RootQuery {
-    events: [String!]!
+    events: [Event!]!
   }
-
   type RootMutation {
-    createEvent(name: String): String
+    createEvent(eventInput: EventInput): Event
   }
-
   schema {
     query: RootQuery
     mutation: RootMutation
@@ -25,19 +37,45 @@ const schema = buildSchema(`
 
 const rootValue = {
   events: () => {
-    return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+    return events
   },
-  createEvent: (args) => {
-    const eventName = args.name
-    return eventName
+  createEvent: args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date)
+    })
+    return event
+      .save()
+      .then(res => {
+        console.log(res)
+        return { ...res._doc, _id: res._doc._id.toString() }
+      })
+      .catch(err => {
+        console.log(err)
+        throw err
+      })
   }
 }
 
-app.use('/graphql', graphqlHttp({
-  schema: schema,
-  rootValue: rootValue,
-  graphiql: true
-}))
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: schema,
+    rootValue: rootValue,
+    graphiql: true
+  })
+)
+
+const MONGO_DB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@graphql-i1v5d.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+mongoose
+  .connect(MONGO_DB_URI, { useNewUrlParser: true })
+  .then(() => {
+    app.listen(4000)
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 
-app.listen(3000)
